0e1c61cb1a33926808fa97a5a45680d90772866f
harrison <851596652@qq.com>
2019-02-12 11:53:52 +0800

    second commit -- like组件基本完成

--- components/like/index.js
+++ components/like/index.js
@@ -4,20 +4,38 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like: {
+      type: Boolean
+    },
+    count: {
+      type: Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    // 数据绑定
+    yesSrc: 'images/like@2x.png',
+    noSrc: 'images/dislike@2x.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    onLike () {
+      let like = this.properties.like
+      let count = this.properties.count
 
+      like = !like
+      count = like?count+1:count-1
+
+      this.setData({
+        count,
+        like
+      })
+    } 
   }
 })
